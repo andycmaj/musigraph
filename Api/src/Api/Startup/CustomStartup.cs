73503605c9484634dcab78663e6698db19b2c771
Andy Cunningham <andycunn@gmail.com>
2018-12-17 00:36:41 -0800

    add debug header output

--- Api/src/Api/Startup/CustomStartup.cs
+++ Api/src/Api/Startup/CustomStartup.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using SerilogEventLogger;
 using SimpleInjector;
 
 namespace Api.Startup
--- ui/config-overrides.js
+++ ui/config-overrides.js
@@ -6,12 +6,25 @@ const path = require('path');
 const createRewire = plugin => (config, env) =>
   injectBabelPlugin(plugin, config);
 
-module.exports = compose(
-  rewireReactHotLoader,
-  rewireAliases.aliasesOptions({
-    // 'react-dom': '@hot-loader/react-dom',
-    'redux-audio': path.resolve(__dirname, `${paths.appSrc}/redux-audio/src`),
-  }),
-  createRewire('@quickbaseoss/babel-plugin-styled-components-css-namespace'),
-  createRewire('styled-components')
-);
+const alias = rewireAliases.aliasesOptions({
+  // 'react-dom': '@hot-loader/react-dom',
+  'redux-audio': path.resolve(__dirname, `${paths.appSrc}/redux-audio/src`),
+});
+
+module.exports =
+  process.env.NODE_ENV === 'production'
+    ? compose(
+        alias,
+        createRewire(
+          '@quickbaseoss/babel-plugin-styled-components-css-namespace'
+        ),
+        createRewire('styled-components')
+      )
+    : compose(
+        rewireReactHotLoader,
+        alias,
+        createRewire(
+          '@quickbaseoss/babel-plugin-styled-components-css-namespace'
+        ),
+        createRewire('styled-components')
+      );
